3eab531b42bb32184d1685697649b268e5818b11
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2021-11-14 23:39:35 +0300

    wip

--- packages/index.ts
+++ packages/index.ts
@@ -1,5 +1,6 @@
 import * as archiver from './archiver';
 import * as core from './core';
 import * as crawler from './crawler';
+export { Timestamp, toTimestamp, toDate } from './utils';
 
 export { archiver, core, crawler };
--- packages/utils.ts
+++ packages/utils.ts
@@ -1,4 +1,40 @@
 import { resolve } from 'path';
+import Long from 'long';
+
+export interface Timestamp {
+  seconds: Long;
+  nanos: 0;
+}
+
+export function toDate(message: Timestamp) {
+  return new Date(
+    (message.seconds as unknown as number) * 1000 + message.nanos / 1000,
+  );
+}
+
+export function toTimestamp(date: Date | string | number): Timestamp {
+  switch (typeof date) {
+    case 'string':
+    case 'number': {
+      const time = new Date(date).getTime();
+      const long = Long.fromNumber(time, true);
+
+      return {
+        seconds: long,
+        nanos: 0,
+      };
+    }
+    default: {
+      const time = date.getTime();
+      const long = Long.fromNumber(time, true);
+
+      return {
+        seconds: long,
+        nanos: 0,
+      };
+    }
+  }
+}
 
 export function buildProtoPath(packageName: string) {
   return resolve(
