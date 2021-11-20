403a4807a80dd7c6c69732f103f77f2698e00052
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2021-11-21 00:14:12 +0300

    fix(date): date to timestamp conversion and vice versa

--- packages/utils.ts
+++ packages/utils.ts
@@ -3,12 +3,13 @@ import Long from 'long';
 
 export interface Timestamp {
   seconds: Long;
-  nanos: 0;
+  nanos: Long;
 }
 
 export function toDate(message: Timestamp) {
   return new Date(
-    (message.seconds as unknown as number) * 1000 + message.nanos / 1000,
+    (message.seconds as unknown as number) * 1000 +
+      (message.nanos as unknown as number) / 1000,
   );
 }
 
@@ -17,20 +18,22 @@ export function toTimestamp(date: Date | string | number): Timestamp {
     case 'string':
     case 'number': {
       const time = new Date(date).getTime();
-      const long = Long.fromNumber(time, true);
+      const seconds = Long.fromNumber(time / 1000, true);
+      const nanos = Long.fromNumber((time % 1000) * 1e6, true);
 
       return {
-        seconds: long,
-        nanos: 0,
+        seconds,
+        nanos,
       };
     }
     default: {
       const time = date.getTime();
-      const long = Long.fromNumber(time, true);
+      const seconds = Long.fromNumber(time / 1000, true);
+      const nanos = Long.fromNumber((time % 1000) * 1e6, true);
 
       return {
-        seconds: long,
-        nanos: 0,
+        seconds,
+        nanos,
       };
     }
   }
